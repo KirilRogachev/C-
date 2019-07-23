9816af6b974b2a8d61c83446b646e13af9ea555b
KirilRogachev <kiril.rogachev@gmail.com>
2019-07-23 14:14:02 +0300

    Update

---
+++ TextProcesing/Substring/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Substring
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string firstString = Console.ReadLine();
+            string SecoundString = Console.ReadLine();
+            firstString = firstString.ToLower();
+            SecoundString = SecoundString.ToLower();
+
+            while (SecoundString.Contains(firstString))
+            {
+            SecoundString = SecoundString.Replace(firstString, string.Empty);
+               
+            }
+            Console.WriteLine(SecoundString);
+          
+
+        }
+    }
+}
