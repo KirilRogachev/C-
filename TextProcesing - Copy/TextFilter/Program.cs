139a0f682b146ed1bcc23e6e4e2794448f1c284c
Rogachev <kiril.rogachev@gmail.com>
2019-12-12 14:00:35 -0800

    n

---
+++ TextProcesing - Copy/TextFilter/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Linq;
+
+namespace TextFilter
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] words= Console.ReadLine()
+                .Split(", ")
+                .ToArray();
+            string text = Console.ReadLine();
+
+            for (int i = 0; i < words.Length; i++)
+            {
+                text = text.Replace(words[i], new string('*',words[i].Length));
+            }
+            Console.WriteLine(text);
+        }
+                  
+    }
+}
