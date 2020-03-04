570644714bc854daf3cd31764e7c4d574cccf8be
Rogachev <kiril.rogachev@gmail.com>
2020-03-04 17:43:41 +0200

    N

---
+++ 12. Functional Programing/FuncPrograming Exercises/CustomMinFunction/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+
+namespace CustomMinFunction
+{
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+
+            int[] numbersArray = Console.ReadLine().Split().Select(int.Parse).ToArray();
+            Func<int[], int> find = x =>
+            {
+                int minNum = int.MaxValue;
+                foreach (var item in numbersArray)
+                {
+                    if (item < minNum)
+                    {
+                        minNum = item;
+                    }
+                }
+                return minNum;
+            };
+            Action<int> print = x => Console.WriteLine(x);
+            print(find(numbersArray));
+
+        }
+    }
+}
